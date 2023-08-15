bb53cc1c6a971aac2129b4fb548a611f5b5a31f6
dan-goodrick <dannyjeee@yahoo.com>
2023-08-15 15:16:31 -0600

    global name

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const COMPLIMENTS = [
   'smashing',
   'lovely',
 ];
-
+let userName;
 // Display the homepage
 app.get('/', (req, res) => {
   res.render('index.html');
@@ -47,19 +47,21 @@ app.get('/hello', (req, res) => {
 
 // Handle the form from /hello and greet the user.
 app.get('/greet', (req, res) => {
+  userName = req.query.name
   res.render('greet.html.njk', {...req.query});
 });
-
+console.log(userName)
 app.get('/game', (req, res) => {
   const ans = req.query.play
   console.log(req.query)
   if (ans === 'no'){
-    res.render('goodbye.html.njk')
+    res.render('goodbye.html.njk', {name:userName})
   } else if (ans === 'yes'){
     res.render('game.html.njk')
   }
 })
 
 app.get("/madlib", (req, res) => {
+  console.log(req.query)
   res.render('madlib.html.njk', {...req.query})
 })
